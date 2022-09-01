617aa7cc3888913d881a112356a440f11f62809c
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-09-01 11:00:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// function should receive 1 argument, an Array of JS object
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function superbowlWin(record){
+  const win = record.find(({result}) => result === 'W');
+  if (win){
+    return win.year;
+  }
+  else{
+    return undefined;
+    
+}  
+}
